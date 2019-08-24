31ca2ac4e5f2191ff7e034d165fa60696f8b2f25
menuka-devinda <menukadevinda@gmail.com>
2019-08-24 14:47:44 -0700

    Create sample rest API
    refactor

--- src/main/java/com/menudev/api/controller/UserController.java
+++ src/main/java/com/menudev/api/controller/UserController.java
@@ -14,10 +14,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.menudev.api.domain.User;
 import com.menudev.api.service.UserService;
+import com.menudev.api.util.ResponseDetail;
 
 @RestController
 @RequestMapping("/users")
@@ -32,6 +34,7 @@ public class UserController {
 	}
 
 	@GetMapping
+	@ResponseBody
 	public ResponseEntity<List<User>> getUsers() {
 		LOG.info("get all users");
 		return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
@@ -39,7 +42,8 @@ public class UserController {
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<User> getUserById(@PathVariable(value = "id") Integer id) throws UnexpectedException {
+	@ResponseBody
+	public ResponseEntity<User> getUserById(@PathVariable(value = "id") final Integer id) throws UnexpectedException {
 		LOG.info("get user by user id: " + id);
 		if (null == id) {
 			throw new IllegalArgumentException("Id is invalid ");
@@ -49,34 +53,33 @@ public class UserController {
 	}
 
 	@PostMapping
-	public ResponseEntity<String> createUser(@RequestBody User user) throws UnexpectedException {
-		// Preconditions.checkNotNull(resource);
+	public ResponseEntity<ResponseDetail> createUser(@RequestBody final User user) throws UnexpectedException {
 		LOG.info("create new user ");
 		User createdUser = userService.createUser(user);
 		if (null != createdUser) {
 			LOG.info("new user with id " + createdUser.getId() + " is created successfully");
-			return new ResponseEntity<String>("User created succefully", HttpStatus.CREATED);
+
+			return new ResponseEntity<>(new ResponseDetail(204,"User created succefully"),HttpStatus.CREATED);
+
 		} else {
 			return  new ResponseEntity<>(null, null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<Void> update(@PathVariable("id") Integer id, @RequestBody User user) {
-		// Preconditions.checkNotNull(resource);
-		// RestPreconditions.checkNotNull(service.getById(resource.getId()));
+	public ResponseEntity<ResponseDetail> update(@PathVariable("id") final Integer id, @RequestBody final User user) {
 		LOG.info("update user with id: " + id);
 		userService.updateUser(id, user);
 		LOG.info("user with id: " + id + " is updated successfully");
-		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+		return 	 new ResponseEntity<>(new ResponseDetail(204,"User modified succefully"),HttpStatus.NO_CONTENT);
 	}
 
 	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<Void> delete(@PathVariable("id") Integer id) {
+	public ResponseEntity<ResponseDetail> delete(@PathVariable("id") final Integer id) {
 		LOG.info("delete user with id: " + id);
 		userService.deleteUser(id);
 		LOG.info("user with id: " + id + " is deleted successfully");
-		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+		return 	 new ResponseEntity<>(new ResponseDetail(204,"User deleted succefully"),HttpStatus.NO_CONTENT);
 	}
 
 }
